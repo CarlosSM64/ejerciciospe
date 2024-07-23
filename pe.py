59b4f982e6d07fe846d7369b884154e1704797b6
Carlos Mantilla <carlos.mantilla@tecnologicoliceocristiano.edu.ec>
2024-07-22 21:14:34 -0500

    first commit

---
+++ pe.py
@@ -0,0 +1,6 @@
+valor = float(input("Ingresa valor a calcular iva: "))
+conIVA = valor * 1.15
+print("Con Iva:", conIVA)
+iva = valor * 0.15
+print("El iva entonces es:", iva)
+print("el porcentaje es:", 1.15)
