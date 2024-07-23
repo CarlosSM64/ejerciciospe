c13321e42eac3339af0cdc68b95b1fb634c7838f
Carlos Mantilla <carlos.mantilla@tecnologicoliceocristiano.edu.ec>
2024-07-22 21:29:53 -0500

    Actividad

---
+++ actividad.py
@@ -0,0 +1,12 @@
+# Escriba un programa que solitice al usuario ingresar tres números
+# para luego mostrarle el promedio de los tres
+
+Numero1 = float(input("Ingrese el primer número: "))
+Numero2 = float(input("ingrese el segundo número: "))
+Numero3 = float(input("Ingrese el tercer número: "))
+
+Resultado = Numero1 + Numero2 + Numero3
+
+Promedio = Resultado/3
+
+print("El promedio es"+ str(Promedio))
